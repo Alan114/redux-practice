e42c18a964ac69fb8269bc8c7af33b814815e5a0
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-10 22:52:39 +0200

    using useSelector to retrieve data from the redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,9 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter);
 
   return (
     <main className={classes.counter}>
